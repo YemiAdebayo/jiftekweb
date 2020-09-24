b04792dddb2f3dc05c60e07f3d5e667f11c3eab8
Adebayo Adeyemi <yemi.adebayo.a@gmail.com>
2020-09-24 11:34:57 +0000

    Completed 'other-services-page'

--- services/urls.py
+++ services/urls.py
@@ -14,22 +14,12 @@ from .views import (cloud_services_view,
                     other_serevices_view
                     )
 
-if settings.DEBUG:
-    urlpatterns = [
+urlpatterns = [
         url(r'^cloud-computing/$', cloud_services_view, name="cloud-computing"),
         url(r'^web-and-app-design/$', web_and_app_view, name="web-and-app-design"),
         url(r'^it-consulting/$', it_consulting_view, name="it-consulting"),
         url(r'^security-and-compliance/$', security_and_compliance_view, name="security-and-compliance"),
         url(r'^training/$', training_serevices_view, name="training"),
         url(r'^other-services/$', other_serevices_view, name="other-services"),
-        ]
-else:
-    urlpatterns = [
-        url(r'^cloud-computing/$', cloud_services_view, name="cloud-computing"),
-        url(r'^web-and-app-design/$', web_and_app_view, name="web-and-app-design"),
-        url(r'^it-consulting/$', it_consulting_view, name="it-consulting"),
-        url(r'^security-and-compliance/$', security_and_compliance_view, name="security-and-compliance"),
-        url(r'^training/$', training_serevices_view, name="training"),
-        url(r'^other-services/$', coming_soon_view, name="other-services"),
         ]
 
